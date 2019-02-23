1d72a077f6fb39ecd586993b64ce7f8d4d659adb
Surya Dantuluri <surya.mvhs@gmail.com>
2019-02-23 13:06:35 -0800

    update

--- end.py
+++ end.py
@@ -13,18 +13,19 @@ import tensorflow as tf
 if __name__ == '__main__':
      import argparse
      parser = argparse.ArgumentParser()
-     parser.add_argument('--cpu', type=int, default=4)
+     parser.add_argument('--cpu', type=int, default=1)
      parser.add_argument('--num_runs', type=int, default=10)
      # parser.add_argument('--clip_ratio', type=int, )
      args = parser.parse_args()
 
-     Enduro = ExperimentGrid(name='vpg-siete')
+     Enduro = ExperimentGrid(name='vpg-ocho')
      Enduro.add('env_name', 'Enduro-ram-v0', '', True)
      # eg.add('clip_ratio', [0.1,0.2])
      Enduro.add('seed', [10*i for i in range(args.num_runs)])
      Enduro.add('epochs', 10)
      Enduro.add('steps_per_epoch', [4000,100])
-     Enduro.add('optimizer',['AdadeltaOptimizer', 'AdagradOptimizer', 'AdamOptimizer', 'FtrlOptimizer', 'GradientDescentOptimizer', 'MomentumOptimizer', 'ProximalAdagradOptimizer', 'ProximalGradientDescentOptimizer', 'RMSPropOptimizer', 'AdaMaxOptimizer', 'AdamGSOptimizer', 'AdamWOptimizer', 'AddSignOptimizer', 'GGTOptimizer', 'LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'])
+     Enduro.add('optimizer',['LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'])
      Enduro.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
      Enduro.add('ac_kwargs:activation', [tf.nn.relu, tf.nn.relu6, tf.nn.crelu, tf.nn.elu, tf.nn.selu, tf.nn.softplus, tf.nn.softsign, tf.sigmoid, tf.tanh], '')
      Enduro.run(vpg, num_cpu=args.cpu)
+     #, 'ProximalAdagradOptimizer', 'ProximalGradientDescentOptimizer', 'RMSPropOptimizer', 'AdaMaxOptimizer', 'AdamGSOptimizer', 'AdamWOptimizer', 'AddSignOptimizer', 'GGTOptimizer', 'LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'
--- walker.py
+++ walker.py
@@ -18,13 +18,26 @@ if __name__ == '__main__':
      # parser.add_argument('--clip_ratio', type=int, )
      args = parser.parse_args()
 
-     BipedalWalkerHardcore = ExperimentGrid(name='vpg-siete')
+     BipedalWalkerHardcore = ExperimentGrid(name='vpg-ocho')
      BipedalWalkerHardcore.add('env_name', 'BipedalWalkerHardcore-v2', '', True)
      # eg.add('clip_ratio', [0.1,0.2])
      BipedalWalkerHardcore.add('seed', [10*i for i in range(args.num_runs)])
      BipedalWalkerHardcore.add('epochs', 10)
      BipedalWalkerHardcore.add('steps_per_epoch', [4000,100])
-     BipedalWalkerHardcore.add('optimizer',['AdadeltaOptimizer', 'AdagradOptimizer', 'AdamOptimizer', 'FtrlOptimizer', 'GradientDescentOptimizer', 'MomentumOptimizer', 'ProximalAdagradOptimizer', 'ProximalGradientDescentOptimizer', 'RMSPropOptimizer', 'AdaMaxOptimizer', 'AdamGSOptimizer', 'AdamWOptimizer', 'AddSignOptimizer', 'GGTOptimizer', 'LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'])
+     BipedalWalkerHardcore.add('optimizer',['LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'])
      BipedalWalkerHardcore.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
      BipedalWalkerHardcore.add('ac_kwargs:activation', [tf.nn.relu, tf.nn.relu6, tf.nn.crelu, tf.nn.elu, tf.nn.selu, tf.nn.softplus, tf.nn.softsign, tf.sigmoid, tf.tanh], '')
      BipedalWalkerHardcore.run(vpg, num_cpu=args.cpu)
+
+     # 'AdadeltaOptimizer', 'AdagradOptimizer', 'AdamOptimizer', 'FtrlOptimizer', 'GradientDescentOptimizer', 'MomentumOptimizer', 'ProximalAdagradOptimizer', 'ProximalGradientDescentOptimizer', 'RMSPropOptimizer', 'AdaMaxOptimizer', 'AdamGSOptimizer', 'AdamWOptimizer', 'AddSignOptimizer', 'GGTOptimizer', 'LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'
+
+     #Done AdadeltaOptimizer, AdagradOptimizer, AdamOptimizer, FtrlOptimizer
+     #Run GradientDescentOptimizer, MomentumOptimizer, ProximalAdagradOptimizer, ProximalGradientDescentOptimizer, RMSPropOptimizer, 'AdamGSOptimizer', 'AdamWOptimizer', 'AddSignOptimizer', 'GGTOptimizer', 'LARSOptimizer', 'LazyAdamGSOptimizer', 'LazyAdamOptimizer', 'MomentumWOptimizer', 'NadamOptimizer', 'PowerSignOptimizer', 'RegAdagradOptimizer', 'ShampooOptimizer'
+
+     # GradientDescentOptimizer does not work with 1 or 2 or 4 CPUs on Enduro, Pacman, Montezuma
+     # GradientDescentOptimizer works on Walker and Lunar
+
+     # MomentumOptimizer, ProximalAdagradOptimizer, ProximalGradientDescentOptimizer, RMSPropOptimizer, AdamGSOptimizer, AdamWOptimizer, AddSignOptimizer same as GradDescent
+     # GGTOptimizer only works on lunar
+
+     #RN: LARSOptimizer,LazyAdamGSOptimizer, rest
